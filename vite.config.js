cf950c3027ec23fe06cf9ebaacbb35cd4b9125cd
Niklas Strand <nikstra@users.noreply.github.com>
2024-01-02 22:52:54 +0100

    Configure base url and outDir.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from "@vitejs/plugin-react";
 export default () => {
   return defineConfig({
     plugins: [react()],
+    base: "https://nikstra.github.io/react-gh-pages-playground",
     server: {
         open: true,
+    },
+    build: {
+        outDir: './build'
     }
  })
 }
