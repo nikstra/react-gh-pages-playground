f7da27f7ae45f609616356c92037b0d9362fad1e
Niklas Strand <nikstra@users.noreply.github.com>
2024-01-02 23:05:49 +0100

    Remove domain part of base url.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default () => {
   return defineConfig({
     plugins: [react()],
-    base: "https://nikstra.github.io/react-gh-pages-playground",
+    base: "/react-gh-pages-playground",
     server: {
         open: true,
     },
